c63f5a64aec39d067966358451589720e5208668
grisentb <36477181+grisentb@users.noreply.github.com>
2018-02-28 15:17:21 +0100

    Update CreateCustomer.java

--- CreateCustomer.java
+++ CreateCustomer.java
@@ -2,10 +2,9 @@ package lab5.state;
 
 public class CreateCustomer {
 	
-	public void CreateCustomer(int n) {
-		for(int i = 1; i <= n; i++) {
-			Customer a = new Customer(i);	
-		}
+	public Customer CreateCustomer(int n) {
+			Customer a = new Customer(i);
+		return a;
 		}
 }
 
