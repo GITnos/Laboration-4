7b0fda007a0fff651c7ee7b14179ead971267e48
SamuelGraden <samuel.graden@gmail.com>
2018-02-28 15:07:22 +0100

    StaticCounter

--- Customer.java
+++ Customer.java
@@ -1,6 +1,7 @@
 package lab5.State;
 
 public class Customer {
+	static private int idCounter =1;
 	private int id;
 	
 	private int arrival;
@@ -10,12 +11,15 @@ public class Customer {
 	Time time;
 	
 	public Customer(int id) {
-		
+		idCounter++;
 		this.id = id;
 		this.arrival = State.arrival();
 		this.gather = State.gather();
 		this.checkout = State.gather();
 		
 		
+	}
+	public int getidCounter() {
+		return idCounter;
 	}
 }
