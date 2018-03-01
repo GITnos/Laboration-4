57ec3d7a0c87e3e53713d8d67e58442848ff0e79
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 13:46:43 +0100

    Update EventEnd.java

--- EventEnd.java
+++ EventEnd.java
@@ -1,5 +1,17 @@
 package lab5.Events;
 
-public class EventEnd {
+import lab5.State.Customer;
+
+public class EventEnd extends Event{
+	
+	@Override
+	public void run(Customer customer){
+		
+	}
+	
+	@Override
+	public double getTime() {
+		return Integer.MAX_VALUE-1;
+	}
 
 }
