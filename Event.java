4ea2ea4c82b268292c7cb0b19b5dfb5974dfdb00
GITnos <31008602+GITnos@users.noreply.github.com>
2018-03-01 13:30:10 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,4 +1,5 @@
 package lab5.Events;
+import lab5.State.Customer;
 /**
  * 
  * @author samuelgraden
@@ -8,12 +9,16 @@ public class Event {
 
 	private int id;
 
-	public void run() {
+	public void run(Customer customer) {
 		
 	}
 
 	public int getId() {
 		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
