f71a81ecb606bfd5641de35bd496bc5524622833
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 13:47:19 +0100

    Update EventStart.java

--- EventStart.java
+++ EventStart.java
@@ -1,9 +1,16 @@
 package lab5.Events;
 
+import lab5.State.Customer;
+
 public class EventStart extends Event{
 
 	@Override
-	public void run(){
-		this.run();
+	public void run(Customer customer){
+		
+	}
+	
+	@Override
+	public double getTime() {
+		return Integer.MIN_VALUE;
 	}
 }
