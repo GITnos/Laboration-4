b4854e043b6a2da7fe6cf01dd825ce802f58b3a3
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 16:14:59 +0100

    Update StateStore.java

--- StateStore.java
+++ StateStore.java
@@ -1,5 +1,21 @@
 package lab5.State;
 
-public class StateStore {
+public class StateStore extends State{
+	
+	final static int maxCustomers = 50;
+	int missedCustomers = 0;
 
+	public StateStore(double dLower, double dUpper, long lSeed, double dLambda) {
+		super(dLower, dUpper,lSeed, dLambda);
+	}
+	
+	public StateStore(double dLower, double dUpper, double dLambda) {
+		super(dLower, dUpper, dLambda);
+	}
+
+	public static int getMaxCustomers(){
+		
+		return maxCustomers;		
+	}
+	
 }
