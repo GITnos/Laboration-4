bee6a9e6765f47eecae19987b7dbef27dff51ba0
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 13:39:51 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -1,10 +1,19 @@
-package lab5.Event;
+package lab5.Events;
+
+import lab5.Sim;
+import lab5.State.Customer;
 
 public class EventCheckout extends Event{
 	
 	@Override
-	public void run() {
+	public void run(Customer customer) {
 		
+	}
+	
+	@Override
+	public double getTime() {
+		int id = this.getId();
+		return Sim.customerList.get(id).getCheckout();
 	}
 
 }
