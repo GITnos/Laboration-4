038c617226f69a48511599f784a856bffeabf79d
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 12:32:25 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -1,5 +1,24 @@
-package lab5.Events;
+package lab5.Event;
 
-public class EventArrive {
+import lab5.State.StateStore;
 
+public class EventArrive extends Event{
+
+	
+	@Override
+	public void run() {
+		
+		StateStore.createCustomer()
+		
+		EventArrive eA = new EventArrive();
+		
+		eA.setID();
+		
+		EventQueue.add(new EventArrive());
+		EventQueue.add(new EventGather());
+		EventQueue.add(new EventCheckout());
+		
+		
+	}
+	
 }
