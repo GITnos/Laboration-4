f5e21c27e0d758e32b1f0fc73e96324ca84da395
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 16:02:22 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -2,12 +2,19 @@ package lab5.Events;
 
 import lab5.Sim;
 import lab5.State.Customer;
+import lab5.State.StateStore;
 
 public class EventArrive extends Event{	
+	
 	@Override
   public void run(Customer customer) {
 		super.run(customer);
-		EventQueue.add(new EventArrive());
+		if(StateStore.getMaxCustomers()>=Sim.customerList.size()) {
+			EventQueue.add(new EventArrive());
+		}else {
+			EventQueue.add(new EventEmptyArrive());
+		}
+		
 		EventQueue.add(new EventGather());
 	}
 	
