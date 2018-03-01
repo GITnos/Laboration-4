9d4d9985ccc20bbbdc33be43369e1565bb33d967
SamuelGraden <samuel.graden@gmail.com>
2018-03-01 13:48:06 +0100

    Update EventQueue.java

--- EventQueue.java
+++ EventQueue.java
@@ -1,31 +1,39 @@
-package lab5.Event;
+package lab5.Events;
 
 import java.util.ArrayList;
+
+import lab5.Sim;
 
 public class EventQueue {
 
 	static ArrayList<Event> EventList = new ArrayList();
-	
+
 	public void EventQueue() {
 		EventList.add(0,new EventStart());
-		
+
 		//EventList.add(new EventEnd());
-		
+
 	}
-	
-	public static void add(Event e) {
-		
-		
+
+	public static boolean add(Event e) {
+
 		for(int i = 1; i<EventList.size();i++) {
+			Event event = EventList.get(i);
+			Event before = EventList.get(i-1);
+			Event next = EventList.get(i+1);
 			
+			double sumTime = event.getTime();
+			double sumTimeBefore = before.getTime();
+			double sumTimeNext = next.getTime();
+			
+			if((sumTime >= sumTimeBefore) && (sumTime <= sumTimeNext)){
+				EventList.add(i,e);
+				return true;
+			}
 		}
-	
-		
-		
-		
-		
-		
-		EventList.add(index, e);
-		
+
+		EventList.add(EventList.size(), e);
+		return true;
+
 	}
 }
