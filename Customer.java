749ce3617b9ed3fe4c7383b786f59b92b537403f
GITnos <31008602+GITnos@users.noreply.github.com>
2018-03-02 12:52:16 +0100

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -6,13 +6,18 @@ package lab5.State;
  */
 public class Customer {
 	static int counterID = 0;
+	// Total number of customers that has visited the store in this Sim.
 	static double arriveTime = 0;
+	// Sum of all arrive times
 	private int id;
 	
 	// Arrival times
 	private double arrival;
+	// Time it takes for this customer to reach the store.
 	private double checkout;
+	// Time it takes for this customer to checkout.
 	private double gather;
+	// Time it takes for this customer to gather their wares.
 	
 	public Customer(double dArrival, double dCheckout, double dGather) {
 		this.id = counterID;
@@ -21,9 +26,7 @@ public class Customer {
 		
 		this.gather = dGather;
 		this.checkout = dCheckout;
-		counterID++;
-		
-		
+		counterID++;	
 	}
 	
 	public int getId() {
@@ -39,7 +42,7 @@ public class Customer {
 		return checkout;
 	}
 	public double getAllTime() {
-		
+		// Returns the total time it took for this customer to go through the Sim.
 		return this.arrival + this.gather + this.checkout;
 		
 	}
