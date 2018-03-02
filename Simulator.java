b1608eecbe87716b04641028b81b2088dcb01b06
SamuelGraden <samuel.graden@gmail.com>
2018-03-02 12:38:52 +0100

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -1,5 +1,23 @@
 package lab5;
 
-public class Simulator {
+import java.util.ArrayList;
 
+import lab5.Events.EventQueue;
+import lab5.State.Customer;
+import lab5.State.StateStore;
+
+public class Sim {
+	EventQueue eventQ = new EventQueue();
+	StateStore state = new StateStore(0, 0, 0);
+
+	public static ArrayList<Customer> customerList;
+	
+	public void runSim() {
+		
+		for(int i = 0; i<eventQ.EventList.size();i++) {
+			
+			eventQ.next().run(state.createCustomer());
+		}
+	}
+	
 }
