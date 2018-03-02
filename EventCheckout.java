c29120634e9fbbf42a5ec3b5667394c268a3627a
SamuelGraden <samuel.graden@gmail.com>
2018-03-02 14:32:03 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -5,18 +5,16 @@ import lab5.State.Customer;
 import lab5.State.StateStore;
 
 public class EventCheckout extends Event{
-	
+	private StateStore state;
 	@Override
-	public void run(Customer customer) {
-		if (StateStore.getCheckouts() > 0){
-			
-		}
+	public void run(StateStore state) {
+		this.state = state;
 	}
 	
 	@Override
 	public double getTime() {
 		int id = this.getId();
-		return Sim.customerList.get(id).getArrival()+Sim.customerList.get(id).getGather();
+		return this.state.getCustomer(this.getId()).getArrival()+this.state.getCustomer(this.getId()).getGather();
 	}
 
 }
