9a25bd68762dc0c08c43bd3b927c8b57aa30e3bd
SamuelGraden <samuel.graden@gmail.com>
2018-03-02 14:31:40 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -1,26 +1,43 @@
 package lab5.Events;
 
 import lab5.Sim;
+import lab5.State.CreateCustomer;
 import lab5.State.Customer;
+import lab5.State.State;
 import lab5.State.StateStore;
 
 public class EventArrive extends Event{	
-	
-	@Override
-  public void run(Customer customer) {
-		super.run(customer);
-		if(StateStore.getMaxCustomers()>=Sim.customerList.size()) {
-			EventQueue.add(new EventArrive());
-		}else {
-			EventQueue.add(new EventEmptyArrive());
-		}
-		
-		EventQueue.add(new EventGather());
+
+	private int id;
+	StateStore state;
+	public EventArrive(StateStore state) {
+		Customer customer = state.createCustomer();
+		this.setId(customer.getId());
 	}
-	
+	@Override
+	public void run(StateStore state) {
+		if(true) {
+
+			if(state.getMaxCustomers()>= state.getCurrentCustomers()) {
+
+				
+				EventQueue.add(new EventArrive(state));
+				EventGather EG = new EventGather();
+				EG.setId(this.id);
+				EventQueue.add(EG);
+
+
+			}else {
+				EventQueue.add(new EventArrive(state));
+				state.addMissedCustomer();
+
+			}
+		}
+	}
+
 	@Override
 	public double getTime() {
 		int id = this.getId();
-		return Sim.customerList.get(id).getArrival();
+		return this.state.getCustomer(this.id).getArrival();
 	}
 }
