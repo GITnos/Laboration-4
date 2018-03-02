64503ff1c34590dd2919683fa2fd1baeb3b310db
GITnos <31008602+GITnos@users.noreply.github.com>
2018-03-02 15:29:40 +0100

    Update CreateCustomer.java

--- CreateCustomer.java
+++ CreateCustomer.java
@@ -3,11 +3,13 @@ package lab5.State;
 public class CreateCustomer {
 // Used to create all customers.
 	
+	private double arrivalSum = 0.0d;
 	private int numCustomers = 0;
 	// Total number of customers.
 	
-	public Customer newCustomer(double dArrival, double dCheckout, double dGather, double dTravelTime) {
-		Customer c = new Customer(dArrival, dCheckout, dGather, numCustomers);
+	public Customer newCustomer(double dArrival, double dCheckout, double dGather) {
+		this.arrivalSum += dArrival;
+		Customer c = new Customer(arrivalSum, dCheckout, dGather, numCustomers);
 		numCustomers++;
 		// Returns a new customer.
 		return c;
