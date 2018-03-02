0cb9442ea6a2a8113d6adcfc6cfb9cbb195d2fd6
SamuelGraden <samuel.graden@gmail.com>
2018-03-02 14:35:14 +0100

    Update StateStore.java

--- StateStore.java
+++ StateStore.java
@@ -1,9 +1,15 @@
 package lab5.State;
 
+import java.util.ArrayList;
+
+import lab5.Sim;
+
 public class StateStore extends State{
 	
-	final static int maxCustomers = 50;
-	int missedCustomers = 0;
+	final int maxCustomers = 50;
+	private int currentCustomers = 0;
+	private int missedCustomers = 0;
+	private ArrayList<Customer> customerList = new ArrayList();
 
 	public StateStore(double dLower, double dUpper, long lSeed, double dLambda) {
 		super(dLower, dUpper,lSeed, dLambda);
@@ -12,10 +18,28 @@ public class StateStore extends State{
 	public StateStore(double dLower, double dUpper, double dLambda) {
 		super(dLower, dUpper, dLambda);
 	}
-
-	public static int getMaxCustomers(){
-		
-		return maxCustomers;		
+	public Customer createCustomer() {
+		Customer customer = new Customer(this.getArrival(),this.getGather(),this.getGather());
+		this.customerList.add(customer);
+		return customer;
+	}
+	public int getMaxCustomers(){	
+		return this.maxCustomers;		
+	}
+	public int getCurrentCustomers() {
+		return this.currentCustomers;
+	}
+	public void addCustomer() {
+		this.currentCustomers++;
+	}
+	public void removeCustomer() {
+		this.currentCustomers--;
+	}
+	public void addMissedCustomer() {
+		this.missedCustomers++;
+	}
+	public Customer getCustomer(int id) {
+		return customerList.get(id);
 	}
 	
 }
