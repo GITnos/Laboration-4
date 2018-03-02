5125d9b178eeac3553d35d288dc154690241a2e1
SamuelGraden <samuel.graden@gmail.com>
2018-03-02 14:34:07 +0100

    Update EventStart.java

--- EventStart.java
+++ EventStart.java
@@ -1,12 +1,13 @@
 package lab5.Events;
 
 import lab5.State.Customer;
+import lab5.State.StateStore;
 
 public class EventStart extends Event{
 
 	@Override
-	public void run(Customer customer){
-		EventQueue.add(new EventArrive());
+	public void run(StateStore state){
+		EventQueue.add(new EventArrive(state));
 	}
 	
 	@Override
