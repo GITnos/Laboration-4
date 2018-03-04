40618a53ee5e8add5c4ee3151265932fc2132208
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:31:09 +0100

    Update EventQueue.java

--- EventQueue.java
+++ EventQueue.java
@@ -3,7 +3,11 @@ package lab5.Events;
 import java.util.ArrayList;
 
 import lab5.Sim;
-
+/**
+ * EventQueue holds every event and keeps them in chronological order.
+ * @author samuelgraden
+ *
+ */
 public class EventQueue {
 
 	public static ArrayList<Event> EventList = new ArrayList();
@@ -14,6 +18,12 @@ public class EventQueue {
 		//EventList.add(new EventEnd());
 
 	}
+	/**
+	 * the event will be added in and sorted after its time.
+	 * 
+	 * @param e the Event which will be added
+	 * @return true when is is done
+	 */
 	public static boolean add(Event e) {
 
 		for(int i = 1; i<EventList.size();i++) {
@@ -35,6 +45,10 @@ public class EventQueue {
 		return true;
 
 	}
+	/**
+	 * 
+	 * @return the next event in line and then removes it
+	 */
 	public Event next() {
 		Event output = EventList.get(0);
 		EventList.remove(0);
