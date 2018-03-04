5e356a20eb8aa585704e0ae8b5d75bedb51a9808
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:08:36 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -5,19 +5,34 @@ import lab5.State.CreateCustomer;
 import lab5.State.Customer;
 import lab5.State.State;
 import lab5.State.StateStore;
-
+/**
+ * EventArrive is the event which is runned when ever a new customer arrives,
+ * @author samuelgraden
+ *
+ */
 public class EventArrive extends Event{	
-
+	/**
+	 * the id is the same as the customer whom which this event belongs to
+	 */
 	private int id;
 	StateStore state;
+	
+	/**
+	 * When an EventArrive is created the customer is created, the customers id is attached to the event.
+	 * @author samuelgraden
+	 * @param state
+	 */
 	public EventArrive(StateStore state) {
 		Customer customer = state.createCustomer();
 		this.setId(customer.getId());
 	}
+	
 	@Override
+	/** run will create the next arrive if there is room for another customer, is will also create its own gatether event
+	 * @param state
+	 */
 	public void run(StateStore state) {
 		if(true) {
-
 			if(state.getMaxCustomers()>= state.getCurrentCustomers()) {
 
 				
@@ -26,7 +41,7 @@ public class EventArrive extends Event{
 				EG.setId(this.id);
 				EventQueue.add(EG);
 
-
+				//the store is full
 			}else {
 				EventQueue.add(new EventArrive(state));
 				state.addMissedCustomer();
@@ -34,8 +49,11 @@ public class EventArrive extends Event{
 			}
 		}
 	}
-
+	
 	@Override
+	/**
+	 * returns the time of the corresponding customer arrive time
+	 */
 	public double getTime() {
 		int id = this.getId();
 		return this.state.getCustomer(this.id).getArrival();
