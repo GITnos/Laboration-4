48f317e6cb935e37e2cb5d92ae64a72281576dec
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:28:25 +0100

    Update EventGather.java

--- EventGather.java
+++ EventGather.java
@@ -2,18 +2,32 @@ package lab5.Events;
 
 import lab5.Sim;
 import lab5.State.Customer;
+import lab5.State.StateStore;
+/**
+ * EventGather is the time is takes to gather weares
+ * @author samuelgraden
+ *
+ */
 
 public class EventGather extends Event{
 	
+	private StateStore state;
+	//Lägg till att det skapas ett checkout event här med samma id.
+	/**
+	*run shall create the checkout event
+	*/
 	@Override
-	public void run(Customer customer) {
-		
+	public void run(StateStore state) {
+		this.state = state;
 	}
-	
+
 	@Override
+	/**
+	 * returns when the customer shall begin gather.
+	 */
 	public double getTime() {
 		int id = this.getId();
-		return Sim.customerList.get(id).getGather();
+		return this.state.getCustomer(this.getId()).getArrival();
 	}
 
 }
