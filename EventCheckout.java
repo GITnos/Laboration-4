63a6daccb46e200dfa0d701878bf1dfbc9d7251d
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:21:49 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -3,14 +3,20 @@ package lab5.Events;
 import lab5.Sim;
 import lab5.State.Customer;
 import lab5.State.StateStore;
-
+/**
+ * EventCheckout 
+ * @author samuelgraden
+ *
+ */
 public class EventCheckout extends Event{
 	private StateStore state;
 	@Override
 	public void run(StateStore state) {
 		this.state = state;
 	}
-	
+	/**
+	 * return the time when the customer is supposed to START checking out.
+	 */
 	@Override
 	public double getTime() {
 		int id = this.getId();
