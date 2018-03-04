14f8fec4de4bc4b021b10c515e3c898a5ca26e07
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:33:47 +0100

    Update EventStart.java

--- EventStart.java
+++ EventStart.java
@@ -3,15 +3,25 @@ package lab5.Events;
 import lab5.State.Customer;
 import lab5.State.StateStore;
 
+/**
+ * Event start is the first event and represents the opening of the store and it creates the first arrive event.
+ * @author samuelgraden
+ *
+ */
 public class EventStart extends Event{
-
 	@Override
+	/**
+	 * EventStart is runned
+	 */
 	public void run(StateStore state){
 		EventQueue.add(new EventArrive(state));
 	}
 	
 	@Override
+	/**
+	 * getTime return the time 0 to represent the start
+	 */
 	public double getTime() {
-		return Integer.MIN_VALUE;
+		return 0;
 	}
 }
