6ba816bb212534b51b0256c0d48f40c916ff676c
SamuelGraden <samuel.graden@gmail.com>
2018-03-04 15:25:25 +0100

    Update EventEnd.java

--- EventEnd.java
+++ EventEnd.java
@@ -2,17 +2,23 @@
 		
 		import lab5.State.Customer;
 		import lab5.State.StateStore;
-		
+		/**
+		 * EventEnd is the last event which will be runned this will be the benchmark for the summary which will be printed out at the end
+		 * @author samuelgraden
+		 *
+		 */
 		public class EventEnd extends Event{
 			
 			@Override
 			public void run(StateStore state){
 				
 			}
-			
+			/**
+			 * the time is max so it allways will be the last.
+			 */
 			@Override
 			public double getTime() {
-				return Integer.MAX_VALUE-1;
+				return Integer.MAX_VALUE;
 			}
 		
 		}
