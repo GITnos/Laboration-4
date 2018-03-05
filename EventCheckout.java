47e053eb70dddfa34aa335bdee79bf3907bdc520
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 21:57:54 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -1,12 +1,9 @@
 package lab5.Events;
 
-import lab5.Sim;
-import lab5.State.Customer;
 import lab5.State.StateStore;
 /**
  * EventCheckout 
- * @author samuelgraden
- *
+ * @author Samuel Gradén
  */
 public class EventCheckout extends Event{
 	private double time;
@@ -14,34 +11,39 @@ public class EventCheckout extends Event{
 	private StateStore state;
 	
 	public EventCheckout(StateStore state,EventQueue EventQueue) {
-		 
 		this.time = state.getCurrentTime();
-		
+		System.out.println("EventCheckout: Created id: " + this.getId());
+		this.state = state;
 	}
-	
+	/**
+	 * When running EventCheckout the Queue time for the customer will be calculated. 
+	 * If there is another customer standing in queue its EventCheckout will be created.
+	 * 
+	 * @author Samuel Gradén, Tom Brander
+	 */
 	@Override
 	public void run(StateStore state,EventQueue EventQueue) {
+		
+		System.out.println("EventCheckout runs id: " + this.getId());
 		super.run(state, EventQueue);
-		this.state = state;
+		//this.state = state;
 		int id = this.getId();
 		state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
 		
-		int newID = state.first().getId();
-		EventCheckout EC = new EventCheckout(state,EventQueue);
-		EC.setId(newID);
-		EventQueue.add(EC);
-		
-		
 		state.removeFirst();
+		
+		if(state.size()>0) {
+			int newID = state.first().getId();
+			EventCheckout EC = new EventCheckout(state,EventQueue);
+			EC.setId(newID);
+			EventQueue.add(EC);
+		}
+		
 	}
-	/**
-	 * return the time when the customer is supposed to START checking out.
-	 */
 	@Override
 	public double getTime() {
 		return this.time;
 		
-		//return this.state.getCustomer(this.getId()).getArrival()+this.state.getCustomer(this.getId()).getGather();
 	}
 
 }
