18da9e568225da93809e41f3aa264c7db524376f
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 21:59:31 +0100

    Update EventGather.java

--- EventGather.java
+++ EventGather.java
@@ -1,36 +1,51 @@
 package lab5.Events;
 
-import lab5.Sim;
-import lab5.State.Customer;
 import lab5.State.StateStore;
+
 /**
  * EventGather is the time is takes to gather weares
- * @author samuelgraden
+ * 
+ * @author Samuel Gradén
  *
  */
+public class EventGather extends Event {
 
-public class EventGather extends Event{
-	
 	private StateStore state;
-	
-	@Override
-	public void run(StateStore state,EventQueue EventQueue) {
-		super.run(state, EventQueue);
+
+	/**
+	 * the constructor of EventGather stores the state
+	 * 
+	 * @author Samuel Gradén
+	 * @param state
+	 */
+	public EventGather(StateStore state) {
+		System.out.println("EventGather: Creating... \nEventGather: Created id:" + this.getId());
 		this.state = state;
-		if(state.getNumOfFreeCounter() >0) {
-			EventCheckout EC = new EventCheckout(state,EventQueue);
-			EC.setId(this.getId());
-		}else{
-			state.add(state.getCustomer(this.getId()));
-		}
 	}
 
 	@Override
 	/**
-	 * returns when the customer shall begin gather.
+	 * @author Samuel Gradén
 	 */
+	public void run(StateStore state, EventQueue EventQueue) {
+		System.out.println("EventGather runs, id:" + this.getId());
+		super.run(state, EventQueue);
+		this.state = state;
+
+		if (state.getNumOfFreeCounter() > 0) {
+			EventCheckout EC = new EventCheckout(state, EventQueue);
+			EC.setId(this.getId());
+			EventQueue.add(EC);
+			
+
+		} else {
+			
+		}
+		state.add(state.getCustomer(this.getId()));
+	}
+
+	@Override
 	public double getTime() {
-		int id = this.getId();
 		return this.state.getCustomer(this.getId()).getArrival();
 	}
 
