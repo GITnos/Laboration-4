388d6fab0df352037c27060150bc5ddfc1516e50
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 14:51:36 +0100

    Update StateStore.java

--- StateStore.java
+++ StateStore.java
@@ -1,5 +1,6 @@
 package lab5.State;
 
+import java.awt.List;
 import java.util.ArrayList;
 
 import lab5.Sim;
@@ -10,19 +11,34 @@ import lab5.Sim;
  */
 public class StateStore extends State{
 	
+
+	private FIFO fifo = new FIFO();
+
+	private double closeTime =  10.00;
+		
+	private int numOfCounter = 4;
+	private int numOfFreeCounter;
+	private double lastCheckedTime = 0;
+
+	private double freeCounterTime = 0;
+
 	final int maxCustomers = 50;
 	private int currentCustomers = 0;
 	private int missedCustomers = 0;
-	
+
+	private ArrayList<ArrayList<Customer>> checkoutQueue = new ArrayList<ArrayList<Customer>>();
+
 	private ArrayList<Customer> customerList = new ArrayList();
 	private CreateCustomer customerCreator = new CreateCustomer();
 
 	public StateStore(double dLower, double dUpper, long lSeed, double dLambda) {
 		super(dLower, dUpper,lSeed, dLambda);
+		this.numOfFreeCounter = this.numOfCounter;
 	}
-	
+
 	public StateStore(double dLower, double dUpper, double dLambda) {
 		super(dLower, dUpper, dLambda);
+		this.numOfFreeCounter = this.numOfCounter;
 	}
 	/**
 	 * createCustomer creates a customer with the CreateCustomer object,
@@ -60,5 +76,49 @@ public class StateStore extends State{
 	public Customer getCustomer(int id) {
 		return customerList.get(id);
 	}
+
+
+	public int getNumOfFreeCounter() {
+		return this.numOfFreeCounter;
+	}
+	public void decreseNumOfFreeCounter() {
+		this.numOfFreeCounter--;
+	}
+	public void increseNumOfFreeCounter() {
+		this.numOfFreeCounter++;
+	}
+
+	public Customer first() {
+		return this.fifo.first();
+	}
+	public void removeFirst() {
+		this.fifo.removeFirst();
+	}
+	public void add(Customer c) {
+		this.fifo.add(c);
+	}
 	
+	public double getLastCheckedTime() {
+		return lastCheckedTime;
+	}
+
+	public void setLastCheckedTime(double lastCheckedTime) {
+		this.lastCheckedTime = lastCheckedTime;
+	}
+
+	public double getFreeCounterTime() {
+		return freeCounterTime;
+	}
+
+	public void setFreeCounterTime(double freeCounterTime) {
+		this.freeCounterTime += freeCounterTime;
+	}
+	public double getCloseTime() {
+		return closeTime;
+	}
+
+	public void setCloseTime(double closeTime) {
+		this.closeTime = closeTime;
+	}
+
 }
