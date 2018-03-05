0d25e14cddba87e6b9274b3304a74ba37ffb0bd1
linoss-7 <36991716+linoss-7@users.noreply.github.com>
2018-03-06 00:03:05 +0100

    Update StateStore.java

--- StateStore.java
+++ StateStore.java
@@ -3,73 +3,60 @@ package lab5.State;
 import java.util.ArrayList;
 
 /**
- * Represent the Store
- * @author Samuel Gradén
+ * 
+ * @author samuelgraden
+ *Represent the Store
  */
 public class StateStore extends State{
 	
+
 	private FIFO fifo = new FIFO();
 
 	private double closeTime =  10.00;
+		
 	private int numOfCounter = 4;
-	
 	private int numOfFreeCounter;
 	private double lastCheckedTime = 0;
 
 	private double freeCounterTime = 0;
 
-	private int maxCustomers = 50;
+	public final static int maxCustomers = 50;
 	private int currentCustomers = 0;
+
 	private int missedCustomers = 0;
 
-	//private ArrayList<ArrayList<Customer>> checkoutQueue = new ArrayList<ArrayList<Customer>>();
+	private ArrayList<ArrayList<Customer>> checkoutQueue = new ArrayList<ArrayList<Customer>>();
 
-	private ArrayList<Customer> customerList;
+	private ArrayList<Customer> customerList = new ArrayList();
 	private CreateCustomer customerCreator = new CreateCustomer();
 
-	
-	
-	public StateStore(double dLower, double dUpper,double pLower, double pUpper, long seed, double dLambda) {
-		super(dLower, dUpper,pLower,pUpper,seed, dLambda);
-		this.customerList = new ArrayList<Customer>();
+	public StateStore(double dLower, double dUpper, double dLambda, int numOfCounter) {
+		super(dLower, dUpper, dLambda);
+		this.numOfCounter = numOfCounter;
 		this.numOfFreeCounter = this.numOfCounter;
 	}
 
-	public StateStore(double dLower, double dUpper,double pLower, double pUpper,  double dLambda) {
-		super(dLower, dUpper,pLower,pUpper, dLambda);
-		this.customerList = new ArrayList<Customer>();
+	public StateStore(double dLower, double dUpper, long lSeed, double dLambda) {
+		super(dLower, dUpper,lSeed, dLambda);
 		this.numOfFreeCounter = this.numOfCounter;
 	}
-	
-	
+
+	public StateStore(double dLower, double dUpper, double dLambda) {
+		super(dLower, dUpper, dLambda);
+		this.numOfFreeCounter = this.numOfCounter;
+	}
 	/**
 	 * createCustomer creates a customer with the CreateCustomer object,
 	 * @return Customer
-	 * @author Samuel Gradén
 	 */
 	public Customer createCustomer() {
 		Customer customer = customerCreator.newCustomer(getArrival(), getCheckout(), getGather());
 		this.customerList.add(customer);
 		return customer;
 	}
-	/**
-	 * 
-	 * @return the maximum amount of customers allowed in the store
-	 */
 	public int getMaxCustomers(){	
 		return this.maxCustomers;		
 	}
-	/**
-	 * Sets the number of customers allowed in the store
-	 * @param n
-	 */
-	public void setMaxCustomers(int n) {
-		this.maxCustomers = n;
-	}
-	/**
-	 * 
-	 * @return the number of customers in the store
-	 */
 	public int getCurrentCustomers() {
 		return this.currentCustomers;
 	}
@@ -80,137 +67,66 @@ public class StateStore extends State{
 		this.currentCustomers++;
 	}
 	/**
-	 * Removes one from the customer count
+	 * removes one from the customer count
 	 */
 	public void removeCustomer() {
 		this.currentCustomers--;
 	}
 	/**
-	 * Adds one to the missed customer counter
+	 * adds one to the missed customer counter
 	 */
 	public void addMissedCustomer() {
 		this.missedCustomers++;
 	}
-	/**
-	 * 
-	 * @return the number of missed customers
-	 */
-	public int getMissedCustomer() {
-		return this.missedCustomers;
-	}
-	/**
-	 * 
-	 * @param id of the customer
-	 * @return the customer with the id
-	 */
+
 	public Customer getCustomer(int id) {
-		//System.out.println("StateStore id:" + id);
 		return customerList.get(id);
 	}
-	/**
-	 * 
-	 * @return the number of free counters
-	 */
+
+	public int getMissedCustomers() { return missedCustomers; }
+
 	public int getNumOfFreeCounter() {
 		return this.numOfFreeCounter;
 	}
-	/**
-	 * Decreases the number of free counters
-	 */
 	public void decreseNumOfFreeCounter() {
 		this.numOfFreeCounter--;
 	}
-	/**
-	 * Increases the number of free counters
-	 */
 	public void increseNumOfFreeCounter() {
 		this.numOfFreeCounter++;
 	}
 
-	/**
-	 * 
-	 * @return the first in line in the queue
-	 */
 	public Customer first() {
 		return this.fifo.first();
 	}
-	/**
-	 * removes the first customer in the queue
-	 */
 	public void removeFirst() {
 		this.fifo.removeFirst();
 	}
-	/**
-	 * Adds a customer to the queue
-	 * @param c
-	 */
 	public void add(Customer c) {
 		this.fifo.add(c);
 	}
-	/**
-	 * 
-	 * @return the size of the queue
-	 */
-	public int size() {
-		return this.fifo.size();
-	}
-	/**
-	 * help method for keeping track the freeCounterTime
-	 * @return time
-	 */
+	
 	public double getLastCheckedTime() {
 		return lastCheckedTime;
 	}
-	
+
 	public void setLastCheckedTime(double lastCheckedTime) {
 		this.lastCheckedTime = lastCheckedTime;
 	}
-	/**
-	 * 
-	 * @return the amount of time the counters have been empty
-	 */
+
 	public double getFreeCounterTime() {
 		return freeCounterTime;
 	}
-	/**
-	 * @param freeCounterTime
-	 */
+
 	public void setFreeCounterTime(double freeCounterTime) {
 		this.freeCounterTime += freeCounterTime;
 	}
-	/**
-	 * @return the time when the store shall close
-	 */
 	public double getCloseTime() {
 		return closeTime;
 	}
-	/**
-	 * 
-	 * @param closeTime
-	 */
+
 	public void setCloseTime(double closeTime) {
 		this.closeTime = closeTime;
 	}
-	/**
-	 * 
-	 * @return number of counters
-	 */
-	public int getNumOfCounter() {
-		return numOfCounter;
-	}
-	/**
-	 * Sets the NumberOfCounter
-	 * @param numOfCounter
-	 */
-	public void setNumOfCounter(int numOfCounter) {
-		this.numOfCounter = numOfCounter;
-	}
-	/**
-	 * 
-	 * @return the size of the queue
-	 */
-	public int listSize() {
-		return this.customerList.size();
-	}
+
 
 }
