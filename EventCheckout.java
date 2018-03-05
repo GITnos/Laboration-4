17a3d2b156e0019ba6f1945269d6709ab1d69854
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 14:33:31 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -9,18 +9,39 @@ import lab5.State.StateStore;
  *
  */
 public class EventCheckout extends Event{
+	private double time;
+	
 	private StateStore state;
+	
+	public EventCheckout(StateStore state,EventQueue EventQueue) {
+		 
+		this.time = state.getCurrentTime();
+		
+	}
+	
 	@Override
-	public void run(StateStore state) {
+	public void run(StateStore state,EventQueue EventQueue) {
+		super.run(state, EventQueue);
 		this.state = state;
+		int id = this.getId();
+		state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
+		
+		int newID = state.first().getId();
+		EventCheckout EC = new EventCheckout(state,EventQueue);
+		EC.setId(newID);
+		EventQueue.add(EC);
+		
+		
+		state.removeFirst();
 	}
 	/**
 	 * return the time when the customer is supposed to START checking out.
 	 */
 	@Override
 	public double getTime() {
-		int id = this.getId();
-		return this.state.getCustomer(this.getId()).getArrival()+this.state.getCustomer(this.getId()).getGather();
+		return this.time;
+		
+		//return this.state.getCustomer(this.getId()).getArrival()+this.state.getCustomer(this.getId()).getGather();
 	}
 
 }
