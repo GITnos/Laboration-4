5f82d067281304a300d513e68f8d4f5117b7648f
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 22:04:10 +0100

    Update CreateCustomer.java

--- CreateCustomer.java
+++ CreateCustomer.java
@@ -1,17 +1,24 @@
 package lab5.State;
-
+/**
+ * Creates customers and keeps track of their id
+ * @author Samuel Gradén
+ *
+ */
 public class CreateCustomer {
-// Used to create all customers.
 	
 	private double arrivalSum = 0.0d;
 	private int numCustomers = 0;
-	// Total number of customers.
-	
+	/**
+	 * 
+	 * @param dArrival
+	 * @param dCheckout
+	 * @param dGather
+	 * @return A Customer
+	 */
 	public Customer newCustomer(double dArrival, double dCheckout, double dGather) {
 		this.arrivalSum += dArrival;
 		Customer c = new Customer(arrivalSum, dCheckout, dGather, numCustomers);
 		numCustomers++;
-		// Returns a new customer.
 		return c;
 	}
 }
