9f6473a5f6ffbc38a4236b23eb56b131a9e0da93
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 21:57:11 +0100

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,8 +1,6 @@
 package lab5.Events;
 import java.util.Observable;
 
-import lab5.State.Customer;
-import lab5.State.State;
 import lab5.State.StateStore;
 /**
  * This class is the general description of an event, all other events build up upon this one
@@ -13,8 +11,10 @@ public class Event extends Observable{
 
 	private int id;
 	/**
-	 * All event have a run method which is runned by the simulation
-	 * @param state
+	 * 
+	 * Every event has a run method which is run by the simulation, run will also update the current time
+	 * @param
+	 * @author Samuel Gradén, Tom Brander
 	 */
 	public void run(StateStore state, EventQueue EventQueue) {
 		state.setFreeCounterTime((state.getCurrentTime()-state.getLastCheckedTime())*state.getNumOfFreeCounter());
@@ -30,7 +30,11 @@ public class Event extends Observable{
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+	/**
+	 * 
+	 * @return the time when this event shall start
+	 * @author Samuel Gradén
+	 */
 	public double getTime() {
 		return 0;
 	}
