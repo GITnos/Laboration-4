04589ac72733920eaedde92fda27f8ec8d75748b
SamuelGraden <samuel.graden@gmail.com>
2018-03-05 21:58:14 +0100

    Update EventClose.java

--- EventClose.java
+++ EventClose.java
@@ -9,13 +9,25 @@ import lab5.State.StateStore;
  */
 public class EventClose extends Event{
 	private double time;
-	
+	/**
+	 *
+	 * @author Samuel Gradén
+	 * @param state
+	 * @param EventQueue
+	 */
 	public EventClose(StateStore state, EventQueue EventQueue) {
 		this.time = state.getCurrentTime();
+		
 	}
+	/**
+	 * When running EventClose, the EventEnd is created. 
+	 * @author Samuel Gradén
+	 */
 	@Override
 	public void run(StateStore state,EventQueue EventQueue) {
 		super.run(state, EventQueue);
+		EventEnd EE = new EventEnd();
+		EventQueue.add(EE);
 	}
 	
 	@Override
