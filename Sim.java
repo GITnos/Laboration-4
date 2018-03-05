664d0fca4b84baa1a526f826b489b5b2e9aff63c
linoss-7 <36991716+linoss-7@users.noreply.github.com>
2018-03-06 00:01:47 +0100

    Update Sim.java

--- Sim.java
+++ Sim.java
@@ -1,9 +1,6 @@
 package lab5;
 
-import java.util.ArrayList;
-
 import lab5.Events.EventQueue;
-import lab5.State.Customer;
 import lab5.State.StateStore;
 /**
  * this creates a simulation
@@ -11,20 +8,27 @@ import lab5.State.StateStore;
  *
  */
 public class Sim {
-	EventQueue eventQ = new EventQueue();
-	StateStore state = new StateStore(0, 0, 0);
+	private EventQueue eventQ;
+	private StateStore state;
+
+	public Sim(int numOfCounter){
+		eventQ = new EventQueue();
+		state = new StateStore((double) 0, (double) 0, (double) 0, numOfCounter);
+	}
+
+	public StateStore getState(){return state;}
 
 	//ArrayList<Customer> customerList;
 	/**
 	 * all of the event is runned until the EventQueue is empty
 	 */
 	public void runSim() {
-		
+
 		while(!(eventQ.isEnd())){
-			
+
 			eventQ.next().run(state,eventQ);
 		}
-		
+
 	}
 	
 }
