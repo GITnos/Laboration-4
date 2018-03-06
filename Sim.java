2d2c323aa8eeb82000813323ec155188f6c30740
SamuelGraden <samuel.graden@gmail.com>
2018-03-06 11:32:29 +0100

    Update Sim.java

--- Sim.java
+++ Sim.java
@@ -1,6 +1,9 @@
 package lab5;
 
+import java.util.ArrayList;
+
 import lab5.Events.EventQueue;
+import lab5.State.Customer;
 import lab5.State.StateStore;
 /**
  * this creates a simulation
@@ -8,27 +11,41 @@ import lab5.State.StateStore;
  *
  */
 public class Sim {
-	private EventQueue eventQ;
-	private StateStore state;
 
-	public Sim(int numOfCounter){
-		eventQ = new EventQueue();
-		state = new StateStore((double) 0, (double) 0, (double) 0, numOfCounter);
+	//deklarera alla parametrar
+	
+	
+	
+	StateStore state;
+	EventQueue eventQ;
+	public Sim(double dLower, double dUpper,double pLower, double pUpper, long seed, double dLambda,int counters,int maxCust) {
+
+		this.eventQ = new EventQueue();
+		this.state = new StateStore(dLower,dUpper,pLower,pUpper,seed,dLambda);
+		state.setNumOfCounter(counters);
+		state.setMaxCustomers(maxCust);
+	}
+	public Sim(double dLower, double dUpper,double pLower, double pUpper, double dLambda,int counters,int maxCust) {
+
+		this.eventQ = new EventQueue();
+		this.state = new StateStore(dLower,dUpper,pLower,pUpper,dLambda);
+		state.setNumOfCounter(counters);
+		state.setMaxCustomers(maxCust);
+		
 	}
 
-	public StateStore getState(){return state;}
 
 	//ArrayList<Customer> customerList;
 	/**
 	 * all of the event is runned until the EventQueue is empty
 	 */
 	public void runSim() {
-
+		
 		while(!(eventQ.isEnd())){
-
+			//System.out.println("Jag kör");
 			eventQ.next().run(state,eventQ);
 		}
 
 	}
-	
+
 }
