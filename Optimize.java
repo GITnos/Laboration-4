c6af4a0e8460c37be67493483017cabf187b7723
linoss-7 <36991716+linoss-7@users.noreply.github.com>
2018-03-06 22:19:24 +0100

    Update Optimize.java

--- Optimize.java
+++ Optimize.java
@@ -1,23 +1,50 @@
 package lab5;
 
-        import lab5.State.StateStore;
+import lab5.State.StateStore;
+import lab5.Events.EventQueue;
 
 /**
  * @author Linnea Ossiansson
  */
 
 public class Optimize {
-    public static int optimize(){
-        int minMissedCustomers = StateStore.maxCustomers;
+    private int maxCustomers = 50;
+
+    private int optimizeOneSim(){
+        int minMissedCustomers = -1;
         int minCounters = 1;
-        for(int i = 1; i <= StateStore.maxCustomers; i++){
-            Sim sim = new Sim(i);
-            sim.runSim();
-            if(sim.getState().getMissedCustomers() < minMissedCustomers){
-                minCounters = i;
-                minMissedCustomers = sim.getState().getMissedCustomers();
+        EventQueue eventQ = new EventQueue();
+        StateStore state = new StateStore(dLower,dUpper,pLower,pUpper,dLambda);
+        for(int i = 1; i <= maxCustomers; i++){
+            state.setNumOfCounter(i);
+
+            while(!(eventQ.isEnd())){
+                eventQ.next().run(state,eventQ);
             }
-        return minCounters;
+
+            if(minMissedCustomers == -1 || state.getMissedCustomer() < minMissedCustomers){
+                minCounters = i;
+                minMissedCustomers = state.getMissedCustomer();
+            }
         }
+        return minCounters;
+
     }
+
+    public int optimizeManySim(){
+        int count = 0;
+        int minCounters;
+        int maxMinCounters = 0;
+        while(count < 100){
+            minCounters = optimizeOneSim();
+            if(minCounters > maxMinCounters){
+                maxMinCounters = minCounters;
+                count = 0;
+            } else {
+                count += 1;
+            }
+        }
+        return maxMinCounters;
+    }
+
 }
