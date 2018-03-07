8e292fa03505ade8ac36d3ce38a286f5dc1f2f68
SamuelGraden <samuel.graden@gmail.com>
2018-03-07 13:44:55 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -4,63 +4,69 @@ import lab5.State.Customer;
 import lab5.State.StateStore;
 /**
  * EventArrive is the event which is runned when ever a new customer arrives,
- * @author samuelgraden
+ * @author Samuel Gradén, Tom Brander
  *
  */
 public class EventArrive extends Event{	
-	/**
-	 * the id is the same as the customer whom which this event belongs to
-	 */
-	//private int id;
+	
 	StateStore state;
 	
 	/**
 	 * When an EventArrive is created the customer is created, the customers id is attached to the event.
-	 * @author samuelgraden
+	 * @author Samuel Gradén
 	 * @param state
 	 */
 	public EventArrive(StateStore state) {
 		//System.out.println("EventArrive: Creating...");
-		System.out.println("=========================================================");
+		//System.out.println("=========================================================");
 		Customer customer = state.createCustomer();
 		
 		this.state = state;
 		this.setId(customer.getId());
-		System.out.println("EventArrive: Created id: " + this.getId());
+		//System.out.println("EventArrive: Created id: " + this.getId());
 	}
 	
 	@Override
 	/** run will create the next arrive if there is room for another customer, is will also create its own gatether event
 	 * @param StateStore EventQueue
+	 * @author Samuel Gradén, Tom Brander
 	 */
 	public void run(StateStore state,EventQueue EventQueue) {
-		//System.out.println(this.state.getCustomer(this.id).getArrival());
+		
+		//System.out.println(this.state.getCustomer(this.getId()).getArrival());
 		state.setCurrentTimte(this.getTime());
-		System.out.println("EventArrive: runs, with id: "+  this.getId());
 		super.run(state, EventQueue);
+		//System.out.println("EventArrive: runs, with id: "+  this.getId());
+		
 		if(state.getCurrentTime()<state.getCloseTime()) {
 			
 			if(state.getMaxCustomers()>= state.getCurrentCustomers()) {
 
-				
+				//System.out.println("HALLLLLLÅÅÅÅ!!!!!");
 				EventQueue.add(new EventArrive(state));
 				EventGather EG = new EventGather(state);
 				EG.setId(this.getId());
+				//System.out.println("EventGather sets id: " + this.getId());
 				EventQueue.add(EG);
 				state.addCustomer();
 
 			//the store is full
 			}else {
-				System.out.println("Butiken är full");
+				//System.out.println("Butiken är full");
 				EventQueue.add(new EventArrive(state));
 				state.addMissedCustomer();
 
 			}
+		
 		}else {
-			System.out.println("Affären stänger;");
+			//System.out.println("Affären stänger;");
 			EventClose EClose = new EventClose(state, EventQueue);
 			EventQueue.add(EClose);
 		}
+	}
+	@Override
+	public String getName() {
+		return "Arrive";
 	}
 	
 	@Override
