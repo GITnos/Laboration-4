9bbff2644aa204a3ddf055438191359b71d95406
SamuelGraden <samuel.graden@gmail.com>
2018-03-07 13:47:41 +0100

    Update EventStart.java

--- EventStart.java
+++ EventStart.java
@@ -5,22 +5,27 @@ import lab5.State.StateStore;
 
 /**
  * Event start is the first event and represents the opening of the store and it creates the first arrive event.
- * @author samuelgraden
+ * @author Samuel Graden
  *
  */
 public class EventStart extends Event{
 	public EventStart() {
-		System.out.println("EventStart: Created");
+		//System.out.println("EventStart: Created");
 	}
 	@Override
 	/**
 	 * runs EventStart
 	 * @param StateStore, EventQueue
+	 * @author Samuel Gradén
 	 */
 	public void run(StateStore state,EventQueue EventQueue){
-		System.out.println("EventStart: Run");
+		//System.out.println("EventStart: Run");
 		super.run(state, EventQueue);
 		EventQueue.add(new EventArrive(state));
+	}
+	@Override
+	public String getName() {
+		return "Start";
 	}
 	
 	@Override
