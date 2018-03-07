9420859f323476e01249f8b7ac69e5a7689602bf
SamuelGraden <samuel.graden@gmail.com>
2018-03-07 13:45:16 +0100

    Update EventArrive.java

--- EventArrive.java
+++ EventArrive.java
@@ -1,59 +1,42 @@
 package lab5.Events;
 
+import lab5.State.Customer;
 import lab5.State.StateStore;
 /**
- * EventCheckout 
- * @author Samuel Gradén, Tom Brander
+ * EventClose shall be runned when the store is closed, this will prohibit further customers to ARRIVE.
+ * @author Samuel Graden, Tom Brander
+ *
  */
-public class EventCheckout extends Event{
+public class EventClose extends Event{
 	private double time;
-	
-	private StateStore state;
-	
-	public EventCheckout(StateStore state,EventQueue EventQueue) {
+	/**
+	 *
+	 * @author Samuel Gradén
+	 * @param state
+	 * @param EventQueue
+	 */
+	public EventClose(StateStore state, EventQueue EventQueue) {
 		this.time = state.getCurrentTime();
-		//System.out.println("EventCheckout: Created id: " + this.getId());
-		this.state = state;
+		
 	}
 	/**
-	 * When running EventCheckout the Queue time for the customer will be calculated. 
-	 * If there is another customer standing in queue its EventCheckout will be created.
-	 * 
+	 * When running EventClose, the EventEnd is created. 
 	 * @author Samuel Gradén, Tom Brander
 	 */
 	@Override
 	public void run(StateStore state,EventQueue EventQueue) {
-		state.setCurrentTimte(this.getTime());
-		//System.out.println("EventCheckout runs id: " + this.getId());
-	//	System.out.println("=========================================================");
 		super.run(state, EventQueue);
-		//this.state = state;
-		int id = this.getId();
-		state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
-		
-		state.removeFirst();
-		state.increseNumOfFreeCounter();
-		
-		if(state.size()>0) {
-			int newID = state.first().getId();
-			EventCheckout EC = new EventCheckout(state,EventQueue);
-			EC.setId(newID);
-			EventQueue.add(EC);
-			state.decreseNumOfFreeCounter();
-			
-		}
-		
+		EventEnd EE = new EventEnd();
+		EventQueue.add(EE);
 	}
 	
 	@Override
 	public String getName() {
-		return "Checkout";
+		return "Close";
 	}
 	
 	@Override
 	public double getTime() {
 		return this.time;
-		
 	}
-
 }
