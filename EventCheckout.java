2f281f59dfb749df6b994e2d50931c4cfa21aaad
SamuelGraden <samuel.graden@gmail.com>
2018-03-07 16:16:45 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -7,12 +7,12 @@ import lab5.State.StateStore;
  */
 public class EventCheckout extends Event{
 	private double time;
-	
+
 	private StateStore state;
-	
+
 	public EventCheckout(StateStore state,EventQueue EventQueue) {
-		this.time = state.getCurrentTime();
 		//System.out.println("EventCheckout: Created id: " + this.getId());
+		this.setTime(state.getCurrentTime());
 		this.state = state;
 	}
 	/**
@@ -23,37 +23,45 @@ public class EventCheckout extends Event{
 	 */
 	@Override
 	public void run(StateStore state,EventQueue EventQueue) {
-		state.setCurrentTimte(this.getTime());
+
+		//state.setCurrentTimte(this.getTime());
 		//System.out.println("EventCheckout runs id: " + this.getId());
-	//	System.out.println("=========================================================");
+		//	System.out.println("=========================================================");
 		super.run(state, EventQueue);
 		//this.state = state;
 		int id = this.getId();
 		state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
+		//System.out.println("CurrentTime:" + state.getCurrentTime()+ " - " + (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()) + " = " + state.getCustomer(id).getQueueTime()+ " "+ id + "  Gather tiden: " + state.getCustomer(id).getGather()); 
 		
-		state.removeFirst();
 		state.increseNumOfFreeCounter();
-		
+	//	System.out.println("Size: " + state.size());
 		if(state.size()>0) {
+			
 			int newID = state.first().getId();
 			EventCheckout EC = new EventCheckout(state,EventQueue);
 			EC.setId(newID);
+			EC.setTime(state.getCurrentTime());
 			EventQueue.add(EC);
 			state.decreseNumOfFreeCounter();
-			
+			state.removeFirst();
+
 		}
+		//System.out.println("CurrentTime:" + state.getCurrentTime());
 		
+		state.removeCustomer();
+
 	}
-	
+	public void setTime(double time ) {
+		this.time = time;
+	}
 	@Override
 	public String getName() {
 		return "Checkout";
 	}
-	
+
 	@Override
 	public double getTime() {
-		return this.time;
-		
+		return state.getCustomer(this.getId()).getArrGatQue();
 	}
 
 }
