c52f16441de34e6b30460f3ab3beafb459ef5898
SamuelGraden <samuel.graden@gmail.com>
2018-03-08 22:29:21 +0100

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -11,8 +11,10 @@ public class EventCheckout extends Event{
 	private StateStore state;
 
 	public EventCheckout(StateStore state,EventQueue EventQueue) {
+		
 		//System.out.println("EventCheckout: Created id: " + this.getId());
-		state.getCustomer(this.getId()).setQueueTime(state.getCurrentTime() - state.getCustomer(this.getId()).getArrGat());
+		state.getCustomer(this.getId()).setQueueTime(state.getCurrentTime() - state.getCustomer(this.getId()).getArrival() -  state.getCustomer(this.getId()).getGather() );
+		this.time = state.getCurrentTime() + state.getCustomer(this.getId()).getCheckout();
 		this.state = state;
 		
 	}
@@ -24,23 +26,24 @@ public class EventCheckout extends Event{
 	 */
 	@Override
 	public void run(StateStore state,EventQueue EventQueue) {
-		//state.setCurrentTimte(this.getTime());
-		//System.out.println("EventCheckout runs id: " + this.getId());
-		//	System.out.println("=========================================================");
+		
+		
 		super.run(state, EventQueue);
+		
+		
 		//this.state = state;
 		int id = this.getId();
-		state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
+		//state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
 		//System.out.println("CurrentTime:" + state.getCurrentTime()+ " - " + (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()) + " = " + state.getCustomer(id).getQueueTime()+ " "+ id + "  Gather tiden: " + state.getCustomer(id).getGather()); 
 		
 		state.increseNumOfFreeCounter();
 	//	System.out.println("Size: " + state.size());
+		state.setCurrentTimte(this.getTime());
 		if(state.size()>0) {
 			
 			int newID = state.first().getId();
 			EventCheckout EC = new EventCheckout(state,EventQueue);
 			EC.setId(newID);
-			EC.setTime(state.getCurrentTime());
 			EventQueue.add(EC);
 			state.decreseNumOfFreeCounter();
 			state.removeFirst();
@@ -49,10 +52,8 @@ public class EventCheckout extends Event{
 		//System.out.println("CurrentTime:" + state.getCurrentTime());
 		
 		state.removeCustomer();
-		state.setCurrentTimte(this.getTime());
-	}
-	public void setTime(double time ) {
-		this.time = time;
+		
+	
 	}
 	@Override
 	public String getName() {
@@ -61,7 +62,7 @@ public class EventCheckout extends Event{
 
 	@Override
 	public double getTime() {
-		return state.getCustomer(this.getId()).getAllTime();
+		return this.time;
 	}
 
 }
