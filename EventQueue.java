fe0073b714482318ce7f4172968b957c32a4cfbb
SamuelGraden <samuel.graden@gmail.com>
2018-03-08 22:31:22 +0100

    Update EventQueue.java

--- EventQueue.java
+++ EventQueue.java
@@ -1,6 +1,7 @@
 package lab5.Events;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
 
 /**
@@ -9,7 +10,7 @@ import java.util.ArrayList;
  * @author Samuel Gradén, Tom Brander
  *
  */
-public class EventQueue {
+public class EventQueue{
 
 	private ArrayList<Event> EventList = new ArrayList<Event>();
 	/**
@@ -19,7 +20,7 @@ public class EventQueue {
 	public EventQueue() {
 		EventList.add(0, new EventStart());
 
-		// EventList.add(new EventEnd());
+		EventList.add(new EventEnd());
 	}
 
 	/**
@@ -29,37 +30,33 @@ public class EventQueue {
 	 * @return True when is is done
 	 */
 	public boolean add(Event e) {
-
-		if (EventList.size() == 0) {
+		if(EventList.size()==0) {
 			EventList.add(e);
-
 			return true;
 		}
-		if (e.getTime() < EventList.get(0).getTime()) {
-			EventList.add(0, e);
-			return true;
-		}
-
-		for (int i = 1; i < this.EventList.size() - 1; i++) {
-			Event event = this.EventList.get(i);
-			Event before = this.EventList.get(i - 1);
-			Event next = this.EventList.get(i + 1);
-
-			double sumTime = event.getTime();
-			double sumTimeBefore = before.getTime();
-			double sumTimeNext = next.getTime();
-
-			if ((sumTime >= sumTimeBefore) && (sumTime < sumTimeNext)) {
-				this.EventList.add(i, e);
-				
+		for(int i = 0; i<EventList.size();i++) {
+			if(e.getTime()<EventList.get(i).getTime()) {
+				EventList.add(i,e);
 				return true;
 			}
 		}
-
-		this.EventList.add(this.EventList.size(), e);
-
+		EventList.add(EventList.size()-1,e);
 		return true;
 
+	}
+
+	public void srt() {
+		//boolean isSorted = false;
+		int size = EventList.size();
+		for(int j = size-1; j>0;j--) {
+			for(int i=0;i<j-1;i++) {
+				Event temp = EventList.get(size-1);
+				EventList.remove(size-1);
+				EventList.add(temp);
+
+			}
+
+		}
 	}
 
 	/**
@@ -68,6 +65,7 @@ public class EventQueue {
 	 * @return The next event
 	 */
 	public Event next() {
+		
 		Event output = this.EventList.get(0);
 		this.EventList.remove(0);
 		return output;
@@ -89,4 +87,25 @@ public class EventQueue {
 			return false;
 		}
 	}
+//	public static void main(String[] args0) {
+//		EventQueue EQ = new EventQueue();
+//
+//		System.out.println("TestQueue");
+//		for(int i = 10; i>0; i--) {
+//			Event test1 = new Event();
+//			test1.setTime((int)(1000*Math.random()));
+//			System.out.println(test1.getName() + " 	Time: " + test1.getTime() );
+//			EQ.add(test1);
+//		}
+//
+//		for(int i = 0; i<EQ.EventList.size();i++) {
+//			System.out.println(EQ.EventList.get(i) + " 	time: " + EQ.EventList.get(i).getTime() );
+//
+//		}
+//
+//
+//
+//
+//	}
+
 }
