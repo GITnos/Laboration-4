712da3f67bb411ce28c5651979d52a8a3e8ff4f4
GITnos <31008602+GITnos@users.noreply.github.com>
2018-04-03 14:42:04 +0200

    Update EventCheckout.java

--- EventCheckout.java
+++ EventCheckout.java
@@ -7,16 +7,21 @@ import lab5.State.StateStore;
  */
 public class EventCheckout extends Event{
 	private double time;
-
 	private StateStore state;
+	private double qtime = 0.0d;
+
+	
 
 	public EventCheckout(StateStore state,EventQueue EventQueue) {
-		
-		//System.out.println("EventCheckout: Created id: " + this.getId());
-		state.getCustomer(this.getId()).setQueueTime(state.getCurrentTime() - state.getCustomer(this.getId()).getArrival() -  state.getCustomer(this.getId()).getGather() );
-		this.time = state.getCurrentTime() + state.getCustomer(this.getId()).getCheckout();
 		this.state = state;
-		
+		double checkoutTime = state.getCustomer(this.getId()).getCheckout();
+		double gatherTime = state.getCustomer(this.getId()).getGather();
+		double arriveTime = state.getCustomer(this.getId()).getArrival();
+		//System.out.println("EventCheckout: Created id: " + this.getId());
+	//	qtime = state.getCurrentTime() - state.getCustomer(this.getId()).getArrival() -  state.getCustomer(this.getId()).getGather();
+		this.time = state.getCurrentTime() + state.getCustomer(this.getId()).getCheckout();
+		qtime = time - checkoutTime - gatherTime - arriveTime;
+		state.addqTime(qtime);
 	}
 	/**
 	 * When running EventCheckout the Queue time for the customer will be calculated. 
@@ -25,29 +30,20 @@ public class EventCheckout extends Event{
 	 * @author Samuel Gradén, Tom Brander
 	 */
 	@Override
-	public void run(StateStore state,EventQueue EventQueue) {
-		
-		
+	public void run(StateStore state,EventQueue EventQueue) {		
 		super.run(state, EventQueue);
-		
-		
-		//this.state = state;
 		int id = this.getId();
-		//state.getCustomer(id).setQueueTime(state.getCurrentTime() - (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()));
-		//System.out.println("CurrentTime:" + state.getCurrentTime()+ " - " + (state.getCustomer(id).getArrival() + state.getCustomer(id).getGather()) + " = " + state.getCustomer(id).getQueueTime()+ " "+ id + "  Gather tiden: " + state.getCustomer(id).getGather()); 
-		
 		state.increseNumOfFreeCounter();
-	//	System.out.println("Size: " + state.size());
 		state.setCurrentTimte(this.getTime());
+		state.addqTime(qtime);
 		if(state.size()>0) {
-			
+			// If there are customers queueing...
 			int newID = state.first().getId();
 			EventCheckout EC = new EventCheckout(state,EventQueue);
 			EC.setId(newID);
 			EventQueue.add(EC);
 			state.decreseNumOfFreeCounter();
 			state.removeFirst();
-
 		}
 		//System.out.println("CurrentTime:" + state.getCurrentTime());
 		
