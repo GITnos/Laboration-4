d0ccbc41547e0e8260bd0d00ac5f897c20bd7aab
GITnos <31008602+GITnos@users.noreply.github.com>
2018-04-03 16:42:54 +0200

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -7,42 +7,17 @@ package lab5.State;
  */
 public class Customer {
 	private int id;
-	private double queueTime = 0;
 	/**
 	 * arrival is the time it takes from the opening of the store to the customer to arrive.
 	 */
-	private double arrival;
-	
-	private double checkout;
-	private double gather;
 
-	public Customer(double dArrival, double dCheckout, double dGather, int iId) {
+	public Customer(int iId) {
 		this.id = iId;
-		this.arrival = dArrival;
-		this.gather = dGather;
-		this.checkout = dCheckout;
 		//System.out.println("Customer created with id: " + this.id);
 	}
 
 	public int getId() {
 		return id;
 	}
-
-	public double getArrival() {
-		return arrival;
-	}
-
-	public double getGather() {
-		return gather;
-	}
-
-	public double getCheckout() {
-		return checkout;
-	}
-	public double getQueueTime() {
-		return this.queueTime;
-	}
-	public void setQueueTime(double time) {
-		this.queueTime = time;
-	}
+	
 }
