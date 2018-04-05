db68aac7c589166505cee26284d077a59ab37914
GITnos <31008602+GITnos@users.noreply.github.com>
2018-04-05 12:59:58 +0200

    Update Optimize.java

--- Optimize.java
+++ Optimize.java
@@ -1,8 +1,11 @@
 package lab5;
 
 import lab5.State.StateStore;
-import lab5.Events.EventQueue;
 
+import java.util.Random;
+
+import lab5.Events.EventQueue;
+import lab5.State.State;
 /**
  * @author Linnea Ossiansson
  */
@@ -11,11 +14,45 @@ public class Optimize {
     /**
      * optimizeOneSim går igenom testar en simulation flera gåner med olika antar kassor och returnerar minsta antal kassor med så få missade kuner som möjligt.
      */
+	private Sim sim;
+		public Optimize(Sim s) {
+			sim = s;
+		}
+		
+		public StateStore metod1(StateStore s) {
+			sim = new Sim(s);
+			sim.runSim();
+			return sim.getState();
+		}
+		
+		public int metod2(double dLower, double dUpper, double pLower, double pUpper, double dLambda) {
+			int c = 1;
+			// Number of counters
+			// Missed customers
+			StateStore s = new StateStore(dLower, dUpper, pLower, pUpper, dLambda, c);
+			while (true) {
+  			s = metod1(s);
+  			if (s.getMissedCustomer()>0) {
+  				s.setNumOfCounter(s.getNumOfCounter()+1);
+  			} else {
+  				break;
+  			}
+			}
+			return s.getNumOfCounter();
+		}
+		
+		public metod3(double dLower, double dUpper, double pLower, double pUpper, double dLambda, long lSeed) {
+			Random r;
+			while (true) {
+				r = Random(lSeed);
+			}
+			metod2(dLower, dUpper, pLower, pUpper, dLambda);
+		}
+		
     private int optimizeOneSim(){
         int minMissedCustomers = -1;
         int minCounters = 1;
         EventQueue eventQ = new EventQueue();
-        StateStore state = new StateStore(dLower,dUpper,pLower,pUpper,dLambda, counters);
         int maxCustomers = state.getMaxCustomers();
         for(int i = 1; i <= maxCustomers; i++){
             state.setNumOfCounter(i);
